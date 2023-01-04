3fed75f980e2e4ab34ed5d2a81e921989d3f42f0
nomso <chinomsokoye@gmail.com>
2023-01-04 00:36:33 +0000

    Write method to determine if boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Method to determine if all boxes can be opened
+Using prototype: def canUnlockAll(boxes)
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Check if boxes can be unlocked
+    """
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
