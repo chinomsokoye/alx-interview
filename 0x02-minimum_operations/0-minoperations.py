d5224a6034677edce719378d0d2cfd146f4f25c1
nomso <chinomsokoye@gmail.com>
2023-01-10 11:49:48 +0000

    Write method that calculates feweste number of operations needed, given n, to result in exactly n H characters in a file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Minimum Operations
+Given num n, write a method that calculates the fewest number of operations
+needed to result in exactly n H characters in a file
+Prototype: def minOperations(n)
+Returns an integer
+if n is impossible to achieve, return 0
+"""
+
+
+def minOperations(n):
+    """
+    Function minOpearations
+    Returns an integer
+    """
+    result = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            result += x
+            n /= x
+        x += 1
+    return result
